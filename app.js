5e88f257f19b21c2881fa0387bce0af323a1bcbf
KoroneSS <bcc385526@hotmail.com>
2024-03-20 15:04:02 +0700

    e

--- app.js
+++ app.js
@@ -9,12 +9,25 @@ app.use(express.static('public'))
 app.use(express.static('node_modules'))
 
 router.get("/",(req,res) => {
-    res.status(200).sendFile(path.join(`${__dirname}/main/homepage.html`))
+    res.status(200).sendFile(path.join(`${__dirname}/html/homepage.html`))
 })
 
 router.get("/signin", (req,res) => {
-    res.status(200).sendFile(path.join(`${__dirname}/signin/signin.html`))
+    res.status(200).sendFile(path.join(`${__dirname}/html/signin.html`))
 })
+
+router.get("/aboutus", (req,res) => {
+    res.status(200).sendFile(path.join(`${__dirname}/html/aboutus.html`))
+})
+
+router.get("/cart", (req,res) => {
+    res.status(200).sendFile(path.join(`${__dirname}/html/cart.html`))
+})
+
+router.get("/search", (req,res) => {
+    res.status(200).sendFile(path.join(`${__dirname}/html/search.html`))
+})
+
 app.listen(port,() =>{
     console.log(`listening on port ${port}`)
 })
